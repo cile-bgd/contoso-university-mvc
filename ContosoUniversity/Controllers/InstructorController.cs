dd85acc1653b52889b1ae525ff51d24942ee9f4e
Sinisa Petkovic <sini.petkovic@gmail.com>
2016-09-08 17:14:56 -0400

    finished part 6

---
+++ ContosoUniversity/Controllers/InstructorController.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+using ContosoUniversity.DataAccess;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /Instructor/
+        // The method accepts optional route data (id)  and a query string parameter (courseID) 
+        // that provide the ID values of the selected instructor and selected course, and passes 
+        // all of the required data to the view. The parameters are provided by the Select 
+        // hyperlinks on the page.
+
+        // http://localhost:1230/Instructor/Index/1?courseID=2021
+        // or
+        // http://localhost:1230/Instructor/Index?id=1&CourseID=2021
+
+        // The URLs are created by ActionLink statements in the Razor view. In the following code, the id parameter matches the default route, so id is added to the route data.
+        // @Html.ActionLink("Select", "Index", new { id = item.PersonID  })
+        // In the following code, courseID doesn't match a parameter in the default route, so it's added as a query string.
+        // @Html.ActionLink("Select", "Index", new { courseID = item.CourseID }) 
+
+        public ActionResult Index(int? id, int? courseID)
+        {
+            var viewModel = new InstructorIndexData();
+            // The code specifies eager loading for the Instructor.OfficeAssignment and the Instructor.Courses navigation property
+            viewModel.Instructors = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses.Select(c => c.Department)) // eager loading for the Course.Department navigation property
+                .OrderBy(i => i.LastName);
+
+            if (id != null)
+            {
+                ViewBag.InstructorID = id.Value;
+                viewModel.Courses = viewModel.Instructors
+                    .Where(i => i.InstructorID == id.Value).Single().Courses;
+            }
+
+            if (courseID != null)
+            {
+                ViewBag.CourseID = courseID.Value;
+                
+                // lazy loading
+                // viewModel.Enrollments = viewModel.Courses
+                //     .Where(x => x.CourseID == courseID).Single().Enrollments;
+                
+                // explicit loading
+                var selectedCourse = viewModel.Courses.Where(x => x.CourseID == courseID).Single();
+                db.Entry(selectedCourse).Collection(x => x.Enrollments).Load();
+                foreach (Enrollment enrollment in selectedCourse.Enrollments)
+                {
+                    db.Entry(enrollment).Reference(x => x.Student).Load();
+                }
+
+                viewModel.Enrollments = selectedCourse.Enrollments;
+            }
+
+            return View(viewModel);
+        }
+
+        //
+        // GET: /Instructor/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        //
+        // GET: /Instructor/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location");
+            return View();
+        }
+
+        //
+        // POST: /Instructor/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Instructors.Add(instructor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.InstructorID);
+            return View(instructor);
+        }
+
+        //
+        // GET: /Instructor/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Instructor instructor = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses)
+                .Where(i => i.InstructorID == id)
+                .Single();
+
+            PopulateAssignedCourseData(instructor);
+
+            return View(instructor);
+        }
+
+        //
+        // POST: /Instructor/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, FormCollection formCollection, string[] selectedCourses)
+        {
+            var instructorToUpdate = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses)
+                .Where(i => i.InstructorID == id)
+                .Single();
+
+            if (TryUpdateModel(instructorToUpdate, "", new string[] { "LastName", "FirstMidName", "HireDate", "OfficeAssignment" }))
+            {
+                try
+                {
+                    if (String.IsNullOrWhiteSpace(instructorToUpdate.OfficeAssignment.Location))
+                    {
+                        instructorToUpdate.OfficeAssignment = null;
+                    }
+
+                    UpdateInstructorCourses(selectedCourses, instructorToUpdate);
+
+                    db.Entry(instructorToUpdate).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DataException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name after DataException and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+
+            PopulateAssignedCourseData(instructorToUpdate);
+
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", id);
+            return View(instructorToUpdate);
+        }
+
+        //
+        // GET: /Instructor/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        //
+        // POST: /Instructor/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Instructor instructor = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Where(i => i.InstructorID == id)
+                .Single();
+
+            instructor.OfficeAssignment = null;
+            db.Instructors.Remove(instructor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void PopulateAssignedCourseData(Instructor instructor)
+        {
+            var allCourses = db.Courses;
+            var instructorCourses = new HashSet<int>(instructor.Courses.Select(c => c.CourseID));
+            var viewModel = new List<AssignedCourseData>();
+            foreach (var course in allCourses)
+            {
+                viewModel.Add(new AssignedCourseData
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+            ViewBag.Courses = viewModel;
+        }
+
+        private void UpdateInstructorCourses(string[] selectedCourses, Instructor instructorToUpdate)
+        {
+            if (selectedCourses == null)
+            {
+                instructorToUpdate.Courses = new List<Course>();
+                return;
+            }
+
+            var selectedCoursesHS = new HashSet<string>(selectedCourses);
+            var instructorCourses = new HashSet<int>
+                (instructorToUpdate.Courses.Select(c => c.CourseID));
+            foreach (var course in db.Courses)
+            {
+                if (selectedCoursesHS.Contains(course.CourseID.ToString()))
+                {
+                    if (!instructorCourses.Contains(course.CourseID))
+                    {
+                        instructorToUpdate.Courses.Add(course);
+                    }
+                }
+                else
+                {
+                    if (instructorCourses.Contains(course.CourseID))
+                    {
+                        instructorToUpdate.Courses.Remove(course);
+                    }
+                }
+            }
+        }
+    }
+}
